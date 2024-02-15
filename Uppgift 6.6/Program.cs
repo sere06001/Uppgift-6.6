3a9e480b1ad8b23785b2d6a6b2509f6a64ec04c7
sere06001 <sere06001@utb.vaxjo.se>
2024-02-15 14:42:59 +0100

    Add project files.

---
+++ Uppgift 6.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace uppgift6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening.");
+            string[] mening = Console.ReadLine().Split();
+            StorSmåBokstav(mening);
+            Console.ReadLine();
+        }
+        static void StorSmåBokstav(string[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a[i] = a[i].ToUpper();
+                    Console.Write(a[i]);
+                }
+                else
+                {
+                    a[i] = a[i].ToLower();
+                    Console.Write(a[i]);
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
